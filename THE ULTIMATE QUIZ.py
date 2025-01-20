94204fa9bde5860881e835ba8ebd8cd2a5293bdb
JonEdCode <monika_gouws@yahoo.co.uk>
2025-01-20 19:05:09 +0000

    Add files via upload

--- THE ULTIMATE QUIZ.py
+++ THE ULTIMATE QUIZ.py
@@ -10,6 +10,11 @@ answer3 = Rect(20,390,280,100)
 answer4 = Rect(340,390,280,100)
 skip = Rect(650,270,120,220)
 timebox = Rect(650,100,120,120)
+questions = []
+question = []
+total = 7
+question_number = 0
+questions_correct = 0
 def draw():
     screen.fill("black")
     screen.draw.filled_rect(messegebox,"black")
@@ -21,13 +26,24 @@ def draw():
     screen.draw.filled_rect(skip,"dark green")
     screen.draw.filled_rect(timebox,"dark blue")
     screen.draw.textbox("welcome to the game!",messegebox,color = "white")
-    screen.draw.textbox("question",questionbox,color = "white")
-    screen.draw.textbox("answer 1",answer1,color = "white")
-    screen.draw.textbox("answer 2",answer2,color = "white")
-    screen.draw.textbox("answer 3",answer3,color = "white")
-    screen.draw.textbox("answer 4",answer4,color = "white")
+    screen.draw.textbox(question[0],questionbox,color = "white")
+    screen.draw.textbox(question[1],answer1,color = "white")
+    screen.draw.textbox(question[2],answer2,color = "white")
+    screen.draw.textbox(question[3],answer3,color = "white")
+    screen.draw.textbox(question[4],answer4,color = "white")
     screen.draw.textbox("skip",skip,color = "white")
     screen.draw.textbox("20",timebox,color = "white")
+def read_question_file(): 
+    global questions,total
+    file = open("questions.txt ","r")
+    questions = file.readlines()
+    total = len(questions)
+    file.close()
+def read_question():
+    global question
+    question = questions[question_number].split(",")
+read_question_file()
+read_question()  
 
 
 def update():
