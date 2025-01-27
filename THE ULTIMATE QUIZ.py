6e6967b7b7e7652dead788a55752f1ab20487410
JonEdCode <monika_gouws@yahoo.co.uk>
2025-01-27 19:09:32 +0000

    Add files via upload

--- THE ULTIMATE QUIZ.py
+++ THE ULTIMATE QUIZ.py
@@ -10,11 +10,14 @@ answer3 = Rect(20,390,280,100)
 answer4 = Rect(340,390,280,100)
 skip = Rect(650,270,120,220)
 timebox = Rect(650,100,120,120)
+listthing = [answer1,answer2,answer3,answer4]
 questions = []
 question = []
 total = 7
 question_number = 0
-questions_correct = 0
+score = 0
+gameEnd = False 
+timeleft = 10
 def draw():
     screen.fill("black")
     screen.draw.filled_rect(messegebox,"black")
@@ -44,6 +47,34 @@ def read_question():
     question = questions[question_number].split(",")
 read_question_file()
 read_question()  
+def on_mouse_down(pos):
+    option = 1
+    for answer in listthing:
+        if answer.collidepoint(pos):
+            if option == int(question[5]):
+                corectanswer()
+            else:
+                gameover()
+        option = option +1
+    if skip.collidepoint(pos):
+        read_question()
+def corectanswer():
+    global score,question_number,timeleft
+    score = score +1 
+    question_number = question_number + 1
+    timeleft = 10
+    if question_number == total:
+        gameover()
+    else:
+        read_question()
+def gameover():
+    global gameEnd , question , timeleft
+    gameEnd = True 
+    question = ["game over","-","-","-","-","-"]
+    timeleft = 0 
+
+
+
 
 
 def update():
